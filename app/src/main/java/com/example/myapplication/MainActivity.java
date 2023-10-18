17128ff7abd786a5739357e774f65f88ef453178
choi9033 <cjy9033@naver.com>
2023-10-18 23:29:46 +0900

    바텀 네비게이션뷰 (인스타 하단바)

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.example.myapplication.Frag1;
+import com.example.myapplication.Frag2;
+import com.example.myapplication.Frag3;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    Frag1 frag1;
+    Frag2 frag2;
+    Frag3 frag3;
+    Frag4 frag4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+
+        //프래그먼트 생성
+        frag1 = new Frag1();
+        frag2 = new Frag2();
+        frag3 = new Frag3();
+        frag4 = new Frag4();
+
+        //제일 처음 띄워줄 뷰를 세팅해줍니다. commit();까지 해줘야 합니다.
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,frag1).commitAllowingStateLoss();
+
+
+        //bottomnavigationview의 아이콘을 선택 했을때 원하는 프래그먼트가 띄워질 수 있도록 리스너를 추가합니다.
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int itemId = menuItem.getItemId();//menu_bottom.xml에서 지정해줬던 아이디 값을 받아와서 각 아이디값마다 다른 이벤트를 발생시킵니다.
+                if (itemId == R.id.tab1) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.main_layout, frag1).commitAllowingStateLoss();
+                    return true;
+                } else if (itemId == R.id.tab2) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.main_layout, frag2).commitAllowingStateLoss();
+                    return true;
+                } else if (itemId == R.id.tab3) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.main_layout, frag3).commitAllowingStateLoss();
+                    return true;
+                }
+                else if (itemId == R.id.tab4) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.main_layout, frag4).commitAllowingStateLoss();
+                    return true;
+                }
+                return false;
+
+            }
+
+        });
+
+
+
+    }
+}
