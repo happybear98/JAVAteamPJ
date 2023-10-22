82bf6433f8ec90025aad51f05f00ee4137ed7c86
Han7755 <gksmfgyrud@naver.com>
2023-10-22 16:36:44 +0900

    GPS 서비스 구현

--- app/src/main/java/com/example/myapplication/GPSService.java
+++ app/src/main/java/com/example/myapplication/GPSService.java
@@ -1,4 +1,101 @@
 package com.example.myapplication;
 
-public class GPSService {
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+
+public class GPSService extends Service{
+        // 위치 업데이트를 처리하는 LocationCallback 객체 생성
+        private final LocationCallback mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                // 위치 업데이트를 받으면, 이 메서드가 호출됨
+                if (locationResult.getLastLocation() != null) {
+                    double latitude = locationResult.getLastLocation().getLatitude();
+                    double longitude = locationResult.getLastLocation().getLongitude();
+                    Log.v("LOCATION_UPDATE", latitude + ", " + longitude);
+
+            }
+        }
+    };
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private void startLocationService() {
+        // 알림 채널 설정
+        String channelId = "location_notification_channel";
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent resultIntent = new Intent();
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), channelId);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("Location Service");
+        builder.setDefaults(NotificationCompat.DEFAULT_ALL);
+        builder.setContentText("Running");
+        builder.setContentIntent(pendingIntent);
+        builder.setAutoCancel(false);
+        builder.setPriority(NotificationCompat.PRIORITY_MAX);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (notificationManager != null && notificationManager.getNotificationChannel(channelId) == null) {
+                NotificationChannel notificationChannel = new NotificationChannel(channelId, "Location Service", NotificationManager.IMPORTANCE_HIGH);
+                notificationChannel.setDescription("This channel is used by location service");
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+        }
+        // 위치 요청을 설정 (4초마다 업데이트, 최소 2초 간격)
+        LocationRequest locationRequest =new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY,4000)
+                .setMinUpdateIntervalMillis(2000)
+                .build();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.getFusedLocationProviderClient(this).requestLocationUpdates(locationRequest, mLocationCallback, Looper.getMainLooper());
+        startForeground(Constants.LOCATION_SERVICE_ID, builder.build());
+    }
+    private void stopLocationService() {
+        // 위치 업데이트를 중지하고 서비스를 종료
+        LocationServices.getFusedLocationProviderClient(this).removeLocationUpdates(mLocationCallback);
+        stopForeground(true);
+        stopSelf();
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent != null) {
+            String action = intent.getAction();
+            if (action != null) {
+                if (action.equals(Constants.ACTION_START_LOCATION_SERVICE)) {
+                    startLocationService();
+                } else if (action.equals(Constants.ACTION_STOP_LOCATION_SERVICE)) {
+                    stopLocationService();
+                }
+            }
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
 }
