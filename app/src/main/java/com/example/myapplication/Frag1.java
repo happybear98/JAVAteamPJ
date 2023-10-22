acf2b10a1695e58cd30f2fe0b69a7af483136105
Han7755 <gksmfgyrud@naver.com>
2023-10-22 20:55:08 +0900

    버튼 동작을 위한 이벤트 리스너 및 함수 정의

--- app/src/main/java/com/example/myapplication/Frag1.java
+++ app/src/main/java/com/example/myapplication/Frag1.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,7 +9,10 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import android.Manifest;
 
 public class Frag1 extends Fragment {
     private View view;
@@ -15,9 +20,28 @@ public class Frag1 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       view = inflater.inflate(R.layout.frag1,container,false);
+        final int REQUEST_CODE_LOCATION_PERMISSION = 1;
+        view = inflater.inflate(R.layout.frag1,container,false);
+        view.findViewById(R.id.GPSStartButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getActivity()!=null&&ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_LOCATION_PERMISSION);
+                } else {
+                    startLocationService();
+                }
+            }
+        });
 
 
        return view;
+    }
+    private void startLocationService() {
+        if (getActivity() != null) {
+            Intent intent = new Intent(getActivity(), GPSService.class);
+            intent.setAction(Constants.ACTION_START_LOCATION_SERVICE);
+            ContextCompat.startForegroundService(getActivity(), intent);
+        }
+
     }
 }
