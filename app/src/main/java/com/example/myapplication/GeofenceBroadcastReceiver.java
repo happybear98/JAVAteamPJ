7a6e77f5b80e36358686e4b00a2909520a4267c5
Han7755 <gksmfgyrud@naver.com>
2023-11-05 17:19:59 +0900

    GeofenceBroadcastReceiver 클래스 정의

---
+++ app/src/main/java/com/example/myapplication/GeofenceBroadcastReceiver.java
@@ -0,0 +1,53 @@
+package com.example.myapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+import com.google.android.gms.location.GeofenceStatusCodes;
+
+import java.util.List;
+
+public class GeofenceBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+
+        if (geofencingEvent.hasError()) {
+            String errorMessage = GeofenceStatusCodes.getStatusCodeString(geofencingEvent.getErrorCode());
+            Log.e("GeofenceErr", errorMessage);
+            return;
+        }
+
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
+                geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+
+            List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
+            String transitionMsg;
+
+            switch (geofenceTransition) {
+                case Geofence.GEOFENCE_TRANSITION_ENTER:
+                    transitionMsg = "Enter";
+                    break;
+                case Geofence.GEOFENCE_TRANSITION_EXIT:
+                    transitionMsg = "Exit";
+                    break;
+                default:
+                    transitionMsg = "-";
+                    break;
+            }
+
+            for (Geofence geofence : triggeringGeofences) {
+                Log.i("geofence", geofence.getRequestId() + " - " + transitionMsg);
+            }
+        } else {
+            Log.e("geofence", "Unknown");
+        }
+    }
+}
