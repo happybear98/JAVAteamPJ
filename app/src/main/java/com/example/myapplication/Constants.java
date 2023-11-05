201a131fefbb8d61385781263e8676414de23237
Han7755 <gksmfgyrud@naver.com>
2023-11-05 17:18:18 +0900

    GEOFENCE_RADIUS_IN_METERS 상수 정의

--- app/src/main/java/com/example/myapplication/Constants.java
+++ app/src/main/java/com/example/myapplication/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
     static final int LOCATION_SERVICE_ID = 175;
     static final String ACTION_START_LOCATION_SERVICE = "startLocationService";
     static final String ACTION_STOP_LOCATION_SERVICE = "stopLocationService";
+
+    public static final float GEOFENCE_RADIUS_IN_METERS = 50;
 }
