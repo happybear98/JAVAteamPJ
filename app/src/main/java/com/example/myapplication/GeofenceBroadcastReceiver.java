fa576b33e99389ff3714f7800f723d95aeaa9c67
Han7755 <gksmfgyrud@naver.com>
2023-11-13 01:28:59 +0900

    Geofence에 입장시 알림기능 추가

--- app/src/main/java/com/example/myapplication/GeofenceBroadcastReceiver.java
+++ app/src/main/java/com/example/myapplication/GeofenceBroadcastReceiver.java
@@ -1,9 +1,12 @@
 package com.example.myapplication;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
 
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingEvent;
@@ -29,6 +32,10 @@ public class GeofenceBroadcastReceiver extends BroadcastReceiver {
 
         int geofenceTransition = geofencingEvent.getGeofenceTransition();
 
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+            Log.i("GeofenceNotification","Notification sended");
+            sendNotification(context, "Geofence Entered", "You have entered a geofence");
+        }
         if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
                 geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
 
@@ -52,5 +59,16 @@ public class GeofenceBroadcastReceiver extends BroadcastReceiver {
         } else {
             Log.e("Geofence", "Unknown");
         }
+    }
+
+    private void sendNotification(Context context, String title, String content) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "CHANNEL_ID")
+                .setSmallIcon(R.drawable.ic_notification_icon)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        notificationManager.notify(0, builder.build());
     }
 }
