a7d123d9148f2132a3891b4c29c0d038d0207428
Han7755 <gksmfgyrud@naver.com>
2023-11-12 23:30:04 +0900

    GeofencingEvent.fromIntent() 메서드에서 항상 Null을 반환하는 문제 수정

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.myapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
-
+import android.Manifest;
 import com.example.myapplication.Frag1;
 import com.example.myapplication.Frag2;
 import com.example.myapplication.Frag3;
@@ -18,6 +21,8 @@ import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingClient;
 import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
@@ -33,49 +38,26 @@ public class MainActivity extends AppCompatActivity {
     Frag4 frag4;
     private GeofencingClient geofencingClient;
     private List<Geofence> geofenceList = new ArrayList<>();
+
     Map<String, Location> geofenceData = new HashMap<>();
     PendingIntent geofencePendingIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, GeofenceBroadcastReceiver.class);
-        geofencePendingIntent= PendingIntent.getBroadcast(
-                this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT|PendingIntent.FLAG_IMMUTABLE);
-        geofencingClient = LocationServices.getGeofencingClient(this);
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+
+        geofencingInit();
+
         //프래그먼트 생성
         frag1 = new Frag1();
         frag2 = new Frag2();
         frag3 = new Frag3();
         frag4 = new Frag4();
 
-        Location location1 = new Location("");
-        location1.setLatitude(37.4215317);
-        location1.setLongitude(-122.084085);
-        geofenceData.put("geofence1", location1);
 
-        Location location2 = new Location("");
-        location2.setLatitude(37.4229999);
-        location2.setLongitude(-122.0850575);
-        geofenceData.put("geofence2", location2);
-        bottomNavigationView = findViewById(R.id.bottomNavi);
-
-        for (Map.Entry<String, Location> entry : geofenceData.entrySet()) {
-            geofenceList.add(new Geofence.Builder()
-                    .setRequestId(entry.getKey())
-                    .setCircularRegion(
-                            entry.getValue().getLatitude(),
-                            entry.getValue().getLongitude(),
-                            Constants.GEOFENCE_RADIUS_IN_METERS
-                    )
-                    .setExpirationDuration(Geofence.NEVER_EXPIRE)
-                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
-                            Geofence.GEOFENCE_TRANSITION_EXIT)
-                    .build());
-        }
         //제일 처음 띄워줄 뷰를 세팅해줍니다. commit();까지 해줘야 합니다.
         getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,frag1).commitAllowingStateLoss();
-
 
         //bottomnavigationview의 아이콘을 선택 했을때 원하는 프래그먼트가 띄워질 수 있도록 리스너를 추가합니다.
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -110,6 +92,31 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    private void geofencingInit(){
+        Intent intent = new Intent(this, GeofenceBroadcastReceiver.class);
+        Location location1 = new Location("");
+        location1.setLatitude(37.4215317);
+        location1.setLongitude(-122.084085);
+        geofenceData.put("geofence1", location1);
+
+
+        for (Map.Entry<String, Location> entry : geofenceData.entrySet()) {
+            geofenceList.add(new Geofence.Builder()
+                    .setRequestId(entry.getKey())
+                    .setCircularRegion(
+                            entry.getValue().getLatitude(),
+                            entry.getValue().getLongitude(),
+                            Constants.GEOFENCE_RADIUS_IN_METERS
+                    )
+                    .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                            Geofence.GEOFENCE_TRANSITION_EXIT)
+                    .build());
+        }
+        geofencePendingIntent= getGeofencePendingIntent();
+        geofencingClient = LocationServices.getGeofencingClient(this);
+        addGeofence();
+    }
     private GeofencingRequest getGeofencingRequest() {
         GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
         builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
@@ -118,15 +125,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
         private PendingIntent getGeofencePendingIntent() {
-            // Reuse the PendingIntent if we already have it.
             if (geofencePendingIntent != null) {
                 return geofencePendingIntent;
             }
             Intent intent = new Intent(this, GeofenceBroadcastReceiver.class);
-            // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when
-            // calling addGeofences() and removeGeofences().
+            intent.setAction("com.example.myapplication.ACTION_GEOFENCE_EVENT");
             geofencePendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.
-                    FLAG_UPDATE_CURRENT);
+                    FLAG_UPDATE_CURRENT| PendingIntent.FLAG_MUTABLE);
             return geofencePendingIntent;
+        }
+        private void addGeofence(){
+            final int REQUEST_CODE_LOCATION_PERMISSION = 1;
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_LOCATION_PERMISSION);
+                return;
+            }
+            geofencingClient.addGeofences(getGeofencingRequest(), getGeofencePendingIntent())
+                    .addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.i("addGeo", "add Success");
+                        }
+                    })
+                    .addOnFailureListener(this, new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.i("addGeo", "add Fail");
+                        }
+                    });
         }
 }
